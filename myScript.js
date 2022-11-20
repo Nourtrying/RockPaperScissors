2e1fc706d9d131faa20955f36fdc7f5d6dba7b0b
nour <nbenmoulehom@gmail.com>
2022-11-20 09:04:45 +0100

    RPS ui

--- myScript.js
+++ myScript.js
@@ -1,55 +1,98 @@
+let userScore = 0;
+let computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const compScore_span = document.getElementById("computer-score");
+const scoreboard_div = document.querySelector('.score-bored')
+const result = document.querySelector('.result')
+const rock = document.getElementById('r')
+const paper = document.getElementById('p')
+const scissors = document.getElementById('s')
 
-let choices = ["ROCK", "PAPER", "SCISSORS"];
+function getCompChoice(){
+  const choices = ['r', 'p', 's'];
+  const rand = Math.floor(Math.random() * 3);
+  return choices[rand];
+}
+function win(userChoice, compChoice){
+  userScore++;
+  userScore_span.textContent = userScore;
+  switch (userChoice + compChoice){
+    case "rs":
+      result.firstElementChild.textContent = "Rock covers scissors, You Win!";
+      break;
+    case "pr":
+      result.firstElementChild.textContent = "Paper covers rock, You Win!";
+      break;
+    case "sp":
+      result.firstElementChild.textContent = "Scissors covers paper, You Win!";
+      break;
+  }
+  
+  
 
-// getting the computer choice!
-function getComputerChoice(){
-    return ComputerChoice = choices[Math.floor(Math.random() * choices.length)];
+
 
 }
 
+function lose(userChoice, compChoice){
+  computerScore++;
+  compScore_span.textContent = computerScore;
+  switch (userChoice + compChoice){
+    case "rp":
+      result.firstElementChild.textContent = "Rock beats paper, You lose!";
+      break;
+    case "ps":
+      result.firstElementChild.textContent = "Paper beats scissors, You lose!";
+      break;
+    case "sr":
+      result.firstElementChild.textContent = "Scissors beats rock, You lose!";
+      break;
+  }
+  
+}
 
-//getting the player choice!
-function getPlayerChoice(){
-    let answer = prompt("ROCK PAPER SCISSORS!");
-    let playerChoice = answer.toUpperCase();
-    while((choices.includes(playerChoice) === false) || (playerChoice === null)){
-        // console.log("You entered an undefined character, enter R, s or P! :)");
-        answer = prompt("You entered an undefined string of characters, enter again!");
-        playerChoice = answer.toUpperCase();
-    }
-    return playerChoice;
+function draw(){
+  result.firstElementChild.textContent = "Tight!";
+  
+}
+
+function game(userChoice){
+  const compChoice = getCompChoice()
+  const name = "david"
+  switch (userChoice + compChoice){
+    case "rs":
+    case "pr":
+    case "sp":
+      win(userChoice, compChoice);
+      break;
+    case "rp":
+    case "ps":
+    case "sr":
+      lose(userChoice, compChoice);
+      break;
+    case "rr":
+    case "pp":
+    case "ss":
+      draw();
+      break;
+  }
+  
+
+}
+
+function main(){
+  rock.addEventListener('click', function(){
+    game('r');
+  })
+
+  paper.addEventListener('click', function(){
+    game('p')
+  })
+
+  scissors.addEventListener('click', function(){
+    game('s')
+  })
 }
 
 
-
-
-function checkwinner(Pchoice, Cchoice){
-    if(Pchoice === Cchoice){
-        return result = "Tie";
-    }
-    else if((Pchoice === "PAPER" && Cchoice === "ROCK") ||
-    (Pchoice === "SCISSORS" && Cchoice === "PAPER") ||
-    (Pchoice === "ROCK" && Cchoice === "SCISSORS")){
-        return  `You win! ${Pchoice} beats ${Cchoice}`;
-    }
-    else{
-        return `You lose! ${Cchoice} beats ${Pchoice}`;
-    }
-}
-
-
-console.log("LET THE GAME BEGIN!");
-
-
-for(let i = 0; i < 5; i++){
-    console.log(i);
-    playerChoice = getPlayerChoice();
-    ComputerChoice = getComputerChoice();
-    console.log(playerChoice);
-    console.log(ComputerChoice);
-    console.log(checkwinner(playerChoice, ComputerChoice));
-}
-
-
-console.log("*****************");
-console.log("Refresh to play again");
+main();
